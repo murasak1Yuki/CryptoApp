16ff4142ffe59113d54b21f6cb08260131adae28
murasak1Yuki <purpleheartas@gmail.com>
2022-08-03 16:49:28 +0300

    Use RefreshDataWorker.kt

--- app/src/main/java/com/example/cryptoapp/domain/CoinRepository.kt
+++ app/src/main/java/com/example/cryptoapp/domain/CoinRepository.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.LiveData
 
 interface CoinRepository {
 
-    fun getCoinInfoList() : LiveData<List<CoinInfo>>
+    fun getCoinInfoList(): LiveData<List<CoinInfo>>
 
     fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo>
 
-    suspend fun loadData()
+    fun loadData()
 }
